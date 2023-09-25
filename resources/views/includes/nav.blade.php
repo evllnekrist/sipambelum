1a69fff81c3e39f6694eb306792b0cb9c8050370
Eve <elinyine@gmail.com>
2023-09-25 11:21:41 +0700

    admin [2]: layout

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,59 @@
+<div class="property_dashboard_navbar">
+    <div class="dash_user_avater">
+        <img src="https://via.placeholder.com/400x400" class="img-fluid avater" alt="">
+        <h6>{{Auth::user()->name}}</h6>
+        <span>{{Auth::user()->role}}</span>
+    </div>
+    
+    <div class="dash_user_menues">
+        <ul>
+            <li>
+                <a href="{{route('admin.training')}}">
+                <i class="fas fa-chalkboard-teacher"></i>Pelatihan
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.trainee')}}">
+                <i class="fas fa-user-graduate"></i>Peserta
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.business')}}">
+                <i class="fas fa-business-time"></i>Bisnis
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.local-potential')}}">
+                <i class="fas fa-seedling"></i>Potensi Lokal
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.page')}}">
+                <i class="fas fa-file"></i>Halaman
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.banner')}}">
+                <i class="fas fa-image"></i>Banner
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.news')}}">
+                <i class="far fa-newspaper"></i>Berita
+                </a>
+            </li>
+            <li>
+                <a href="{{route('admin.user')}}">
+                <i class="fas fa-users"></i>Pengguna
+                </a>
+            </li>
+        </ul>
+    </div>
+    
+    <div class="dash_user_footer">
+        <ul>
+            <li title="logout?"><a href="{{route('logout')}}"><i class="fa fa-power-off"></i></a></li>
+            <li><a href="{{route('profile.edit')}}"><i class="fa fa-cog"></i></a></li>
+        </ul>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,28 +13,41 @@
     </head>
     <body class="yellow-skin @yield('body-class')">
       @include('includes.nav-public')
-      <div class="preloader"></div>
-      @yield('content')
-      @include('includes.footer')
-      <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
-      <script>
-        const assetUrl = "{{asset('/')}}"
-      </script>
-      <script src="{{asset('assets/js/jquery.min.js')}}"></script>
-      <script src="{{asset('assets/js/popper.min.js')}}"></script>
-      <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-      <script src="{{asset('assets/js/ion.rangeSlider.min.js')}}"></script>
-      <script src="{{asset('assets/js/select2.min.js')}}"></script>
-      <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
-      <script src="{{asset('assets/js/slick.js')}}"></script>
-      <script src="{{asset('assets/js/slider-bg.js')}}"></script>
-      <script src="{{asset('assets/js/lightbox.js')}}"></script> 
-      <script src="{{asset('assets/js/imagesloaded.js')}}"></script>
-      <script src="{{asset('assets/js/daterangepicker.js')}}"></script>
-      <script src="{{asset('assets/js/custom.js')}}"></script>
-      <script src="{{asset('assets/js/axios.min.js')}}"></script>
-      <script src="{{asset('assets/plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
-      <script src="{{asset('assets/plugins/moment/moment-with-locales.min.js')}}"></script>
-      @yield('addition_script')
+        <div class="preloader"></div>
+        <div id="main-wrapper">
+            <section class="gray pt-5 pb-5">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 col-md-3 col-sm-4">
+                            @include('includes.nav')
+                        </div>
+						<div class="col-lg-10 col-md-9 col-sm-6">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </section>	
+        </div>
+        @include('includes.footer')
+        <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+        <script>
+            const assetUrl = "{{asset('/')}}"
+        </script>
+        <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+        <script src="{{asset('assets/js/popper.min.js')}}"></script>
+        <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('assets/js/ion.rangeSlider.min.js')}}"></script>
+        <script src="{{asset('assets/js/select2.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{asset('assets/js/slick.js')}}"></script>
+        <script src="{{asset('assets/js/slider-bg.js')}}"></script>
+        <script src="{{asset('assets/js/lightbox.js')}}"></script> 
+        <script src="{{asset('assets/js/imagesloaded.js')}}"></script>
+        <script src="{{asset('assets/js/daterangepicker.js')}}"></script>
+        <script src="{{asset('assets/js/custom.js')}}"></script>
+        <script src="{{asset('assets/js/axios.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/sweetalert2/sweetalert2.all.min.js')}}"></script>
+        <script src="{{asset('assets/plugins/moment/moment-with-locales.min.js')}}"></script>
+        @yield('addition_script')
 	</body>
 </html>
