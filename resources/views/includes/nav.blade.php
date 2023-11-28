d5db8b89178fcb36d2eb0fa4ddddaa5a10e94e2b
Eve <elinyine@gmail.com>
2023-11-28 13:22:49 +0700

    hak akses

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -6,56 +6,76 @@
     </div>
     <div class="dash_user_menues">
         <ul>
+            @if(in_array(Auth::user()->role,['70x7','opd','kec']))
             <li>
                 <a href="{{route('admin.training')}}">
                 <i class="fas fa-chalkboard-teacher"></i>Pelatihan
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7','opd','kec']))
             <li>
                 <a href="{{route('admin.trainee')}}">
                 <i class="fas fa-user-graduate"></i>Peserta
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7','opd','kec']))
             <li>
                 <a href="{{route('admin.business')}}">
                 <i class="fas fa-business-time"></i>Bisnis
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7']))
             <li>
                 <a href="{{route('admin.subdistrict')}}">
                 <i class="fas fa-map"></i>Kecamatan
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7']))
             <li>
                 <a href="{{route('admin.local-potential')}}">
                 <i class="fas fa-seedling"></i>Potensi Lokal
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7']))
             <li>
                 <a href="{{route('admin.page')}}">
                 <i class="fas fa-file"></i>Halaman
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7','opd']))
             <li>
                 <a href="{{route('admin.banner')}}">
                 <i class="fas fa-image"></i>Banner
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7','opd']))
             <li>
                 <a href="{{route('admin.news')}}">
                 <i class="far fa-newspaper"></i>Berita
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7','opd']))
             <li>
                 <a href="{{route('register-list')}}">
                 <i class="fas fa-users"></i>Pengguna Web
                 </a>
             </li>
+            @endif
+            @if(in_array(Auth::user()->role,['70x7']))
             <li>
                 <a href="{{route('admin.config')}}">
                 <i class="fas fa-wrench"></i>Konfigurasi
                 </a>
             </li>
+            @endif
         </ul>
     </div>
     
